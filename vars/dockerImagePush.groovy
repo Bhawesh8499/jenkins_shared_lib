59e67215f2e2701f60ce5ffb4b8ebc1f0cfa7350
Bhawesh Parate <110228782+Bhawesh8499@users.noreply.github.com>
2023-12-08 10:28:11 +0530

    Update dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -4,7 +4,9 @@ def call(String project, String ImageTag, String hubUser){
             usernameVariable: "USER",
             passwordVariable: "PASS"
     )]) {
-        sh "docker login -u '$USER' -p '$PASS'"
+       // sh "docker login -u '$USER' -p '$PASS'"
+        sh "echo 'your_password' | docker login -u your_username --password-stdin"
+
     }
     //sh "docker image push ${hubUser}/${project}:${ImageTag}"
     sh "docker image push ${hubUser}/${project}:latest"   
